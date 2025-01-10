fc97c632617180ac2f438a5a6aa0182e23d0e2ea
Maria Manzano Roldán <2023_maria.manzano@iticbcn.cat>
2025-01-10 09:59:16 +0100

    03-Sleep_i_Coet acabat

--- 03-Sleep_i_coet/src/Coet.java
+++ 03-Sleep_i_coet/src/Coet.java
@@ -1,43 +1,42 @@
 public class Coet {
-
     private Motor[] motors = new Motor[4];
 
     public Coet() {
         for (int i = 0; i < motors.length; i++) {
-            motors[i] = new Motor();
+            motors[i] = new Motor(i);
         }
     }
 
     public void passaAPotencia(int p) {
         if (p >= 0 && p <= 10) {
+            System.out.println("Passant a potència: " + p);
+
             for (int i = 0; i < motors.length; i++) {
                 motors[i].setPotenciaObj(p);
             }
-        } else {
-            System.out.println("El valor de la potencia ha de ser entre 0 i 10");
-        }
-    }    
 
-    public void arrenca() {
-        for (int i = 0; i < motors.length; i++) {
-            motors[i].start();
+            for (int i = 0; i < motors.length; i++) {
+                try {
+                    motors[i].join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            System.out.println("El valor de la potència ha de ser entre 0 i 10");
         }
     }
 
     public static void main(String[] args) {
         Coet coet = new Coet();
-
-        coet.arrenca();
-
-        int potencia = Integer.parseInt(Entrada.readLine());
-        coet.passaAPotencia(potencia);
-        coet.arrenca();
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.out.println("Error " + e);
+        int potencia = -1;
+        while (potencia != 0) {
+            try {
+                potencia = Integer.parseInt(Entrada.readLine());
+                coet.passaAPotencia(potencia);
+            } catch (NumberFormatException e) {
+            
+            }
         }
-        coet.passaAPotencia(0);
     }
 }
--- 03-Sleep_i_coet/src/Motor.java
+++ 03-Sleep_i_coet/src/Motor.java
@@ -1,57 +1,53 @@
 public class Motor extends Thread {
     private int potenciaObj = 0;
     private int potenciaAct = 0;
+    private int id;
+
+    public Motor(int id) {
+        this.id = id;
+    }
 
     public int getPotenciaObj() {
         return potenciaObj;
     }
 
     public void setPotenciaObj(int p) {
-        if (p == 0) {
+        if (p < 0 || p > 10) {
+            System.out.printf("La potència ha de ser entre 0 i 10.\n");
+            return;
+        }
+
+        if (p == 0 && potenciaAct > 0) {
             while (potenciaAct > 0) {
                 potenciaAct--;
-                int temps = (int) (Math.random() * 1001) + 1000;
+                System.out.printf("Motor %d: Decre. Objectiu: 0 Actual: %d\n", id, potenciaAct);
                 try {
+                    int temps = (int) (Math.random() * 1001) + 1000;
                     Thread.sleep(temps);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            System.out.printf("Motor %d: Aturat. Objectiu: 0 Actual: %d\n", id, potenciaAct);
             return;
         }
-
         this.potenciaObj = p;
         start();
     }
 
     @Override
     public void run() {
-        if (potenciaAct == 0) {
-            while (potenciaAct > 0) {
-                potenciaAct--;
-                System.out.println("Motor " + this.getName() + " : Decre. Objectiu: 0 Actual: " + potenciaAct);
-                int temps = (int) (Math.random() * 1001) + 1000;
-                try {
-                    Thread.sleep(temps);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            System.out.println("Motor " + this.getName() + ": FerRes Objectiu: 0 Actual: " + potenciaAct);
-        }
-
         while (potenciaAct != potenciaObj) {
             if (potenciaAct < potenciaObj) {
                 potenciaAct++;
-                System.out.println("Motor " + this.getName() + ": Incre. Objectiu: " + potenciaObj + " Actual: " + potenciaAct);
-
+                System.out.printf("Motor %d: Incre. Objectiu: %d Actual: %d\n", id, potenciaObj, potenciaAct);
             } else {
                 potenciaAct--;
-                System.out.println("Motor " + this.getName() + ": Decre. Objectiu: " + potenciaObj + " Actual: " + potenciaAct);
-
+                System.out.printf("Motor %d: Decre. Objectiu: %d Actual: %d\n", id, potenciaObj, potenciaAct);
             }
-            int temps = (int) (Math.random() * 1001) + 1000;
+
             try {
+                int temps = (int) (Math.random() * 1001) + 1000;
                 Thread.sleep(temps);
             } catch (InterruptedException e) {
                 e.printStackTrace();
